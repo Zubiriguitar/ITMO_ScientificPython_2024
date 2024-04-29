e7e7c60208995e8866cdd36a9667b8cb14d1f4c7
Zubiriguitar <ponomarevpavel1995@gmail.com>
2024-04-30 02:33:17 +0700

    Fixed HW3

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -3,64 +3,82 @@ class IncreaseSpeed:
         self.current_speed = current_speed
         self.max_speed = max_speed
 
-    def increase(self):
+    def __iter__(self):
+        return self
+
+    def __next__(self):
         if self.current_speed < self.max_speed:
             self.current_speed += 10
+            if self.current_speed > self.max_speed:
+                self.current_speed = self.max_speed
             return self.current_speed
         else:
-            return None
+            raise StopIteration
 
 
 class DecreaseSpeed:
     def __init__(self, current_speed):
         self.current_speed = current_speed
 
-    def decrease(self):
+    def __iter__(self):
+        return self
+
+    def __next__(self):
         if self.current_speed > 0:
             self.current_speed -= 10
+            if self.current_speed < 0:
+                self.current_speed = 0
             return self.current_speed
         else:
-            return None
+            raise StopIteration
 
 
-class Car():
+class Car:
     cars = 0
     on_road = True
+
     def __init__(self, max_speed: int, current_speed=0):
         Car.cars += 1
         self.max_speed = max_speed
         self.current_speed = current_speed
-        self.gaz = IncreaseSpeed(current_speed, max_speed)
-        self.tormoz = DecreaseSpeed(current_speed)
 
-    def accelerate(self, upper_border=None):
+    def accelerate(self, border=None):
+        speed = self.current_speed
         if not Car.on_road:
             print("Car is currently parked. Cannot accelerate.")
             return
 
-        if upper_border:
-            while True:
-                speed = self.gaz.increase()
-                if speed is None or speed >= upper_border:
-                    break
+        gaz = IncreaseSpeed(self.current_speed, self.max_speed)
+        if border is not None and self.current_speed < border:
+            for speed in gaz:
                 print("Current speed:", speed)
-        else:
-            print("Current speed:", self.gaz.increase())
+                self.current_speed = speed
+                if speed == border:
+                    self.current_speed = border
+                    break
+        elif border is None and self.current_speed < self.max_speed:
+            speed = next(gaz)
+            print("Current speed:", speed)
+            self.current_speed = speed
 
-    def brake(self, lower_border=None):
+    def brake(self, border=None):
+        speed = self.current_speed
         if not Car.on_road:
             print("Car is currently parked. Cannot brake.")
             return
 
-        if lower_border:
-            while True:
-                speed = self.tormoz.decrease()
-                if speed is None or speed <= lower_border:
-                    break
+        tormoz = DecreaseSpeed(self.current_speed)
+        if border is not None and self.current_speed > border:
+            for speed in tormoz:
                 print("Current speed:", speed)
-        else:
-            print("Current speed:", self.tormoz.decrease())
-
+                self.current_speed = speed
+                if speed == border:
+                    self.current_speed = speed
+                    break
+        elif border is None and self.current_speed > 0:
+            speed = next(tormoz)
+            print("Current speed:", speed)
+            self.current_speed = speed
     @classmethod
     def parking(cls, car):
         if  car.on_road:
@@ -74,7 +92,7 @@ class Car():
     def total_cars():
         print("Total number of cars on the road:", Car.cars)
 
-    
+
 
     def show_weather():
         import openmeteo_requests
@@ -89,46 +107,44 @@ class Car():
           "timezone": "Europe/Moscow"
           }
         response = openmeteo.weather_api(url, params=params)[0]
-   
+
         current = response.Current()
         current_temperature_2m = current.Variables(0).Value()
         current_apparent_temperature = current.Variables(1).Value()
         current_rain = current.Variables(2).Value()
         current_wind_speed_10m = current.Variables(3).Value()
-        
+
         return print(f"Current time: {datetime.datetime.fromtimestamp(current.Time()-response.UtcOffsetSeconds())} {response.TimezoneAbbreviation().decode()}\n"
-                     f"Current temperature: {round(current_temperature_2m, 0)} C\n"
-                     f"Current apparent_temperature: {round(current_apparent_temperature, 0)} C\n"
-                     f"Current rain: {current_rain} mm\n"
-                     f"Current wind_speed: {round(current_wind_speed_10m, 1)} m/s")
-
-
-oka = Car(max_speed=120, current_speed=50)
-oka.accelerate(100)
-oka.brake(30)
-
-uaz = Car(max_speed=150)
-uaz.accelerate()
-uaz.accelerate(160)
-uaz.brake()
-
-Car.show_weather()
-
-oka = Car(1,1)
-uaz = Car(1,1)
-niva = Car(1,1)
-
-
-Car.total_cars()
-
-
-Car.parking(oka)
-Car.parking(uaz)
-
-
-Car.total_cars()
+                      f"Current temperature: {round(current_temperature_2m, 0)} C\n"
+                      f"Current apparent_temperature: {round(current_apparent_temperature, 0)} C\n"
+                      f"Current rain: {current_rain} mm\n"
+                      f"Current wind_speed: {round(current_wind_speed_10m, 1)} m/s")
 
 
 
+# oka = Car(max_speed=120, current_speed=50)
+# oka.accelerate(100)
+# oka.brake(30)
+
+# uaz = Car(max_speed=150)
+# uaz.accelerate()
+# uaz.accelerate(160)
+# uaz.brake()
+
+# Car.show_weather()
+
+# oka = Car(1,1)
+# uaz = Car(1,1)
+# niva = Car(1,1)
+
+
+# Car.total_cars()
+
+
+# Car.parking(oka)
+# Car.parking(uaz)
+
+
+# Car.total_cars()
 
 
